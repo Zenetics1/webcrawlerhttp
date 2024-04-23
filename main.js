6ad11b66cce1daa10a98e13d3a23120d5e25a868
Zenetics1 <williamzanderkim@gmail.com>
2024-04-23 10:18:10 -0400

    Restart

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-const { crawlPage } = require('./crawl.js')
+const { default: puppeteer } = require('puppeteer/src/puppeteer.js')
+const puppeteer = require("puppeteer")
+const Crawler = require("./Crawler")
 
-function main(){
-    if(process.argv.length < 3){
-        console.log("no website provided")
-        process.exit(1)
-    }
-    if(process.argv.length > 3){
-        console.log("too many command line args")
-        process.exit(1)
-    }
-    const baseURL = process.argv[2]
+(async () => {
+    // Start up the browser and pages,
+    const browser = await puppeteer.launch();
+    const pages = await browser.newPage();
 
-    console.log(`starting crawl of ${baseURL}`)
-    const pages = crawlPage(baseURL, baseURL, {})
+    const crawl = new Crawler({
 
-    for (const page of Object.entries(pages))
-    {
-        console.log(page)
-    }
-}
+    })
 
-main()
+
+})
