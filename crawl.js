01f335b4d989255fd739367e1d4e7c47b8870321
Zenetics1 <williamzanderkim@gmail.com>
2024-04-23 09:43:16 -0400

    Need to fix and define baseURL

--- crawl.js
+++ crawl.js
@@ -1,7 +1,6 @@
 const {JSDOM} = require('jsdom')
 
 async function crawlPage(currentURL){
-    console.log(`actively crawling: ${currentURL}`)
     
     const baseURLObj = new URL(baseURL)
     const currentURLObj = new URL(currentURL)
@@ -16,22 +15,33 @@ async function crawlPage(currentURL){
         return pages
     }
 
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`actively crawling: ${currentURL}`)
+
     try{
         const resp = await fetch(baseURL, currentURL, pages)
         
         if(resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
 
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType}, on page ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        nextURLs = getURLSFromHTML(htmlBody, baseURL)
+        for(const nextURL of nextURLs)
+        {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+        return pages
 
     } catch (err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
